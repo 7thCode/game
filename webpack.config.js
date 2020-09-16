1e86bea6097c60075520096170eb3facd06df525
MIKIO ODA <oda.mikio@gmail.com>
2020-09-16 12:14:35 +0900

    part13

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/main12.ts',
+    entry: './src/main13.ts',
     mode: 'development',
     output: {
         path: path.resolve(__dirname, 'public/javascripts'),
-        filename: 'main12.js'
+        filename: 'main13.js'
+    },
+
+    module: {
+        rules: [
+          { test: /\.ts$/, loader: "ts-loader", exclude: "/node_modules/" }
+        ]
     },
 
     resolve: {
-        extensions: ['.js']
+        extensions: ['.js', '.ts']
     }
 
 };
